d00eabed2b3631feb3c1062c2511f2b6965250d3
Icorrea7 <isacorrea07@gmail.com>
2018-03-05 20:55:42 -0500

    segundo commit

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
-
-get '/' do
-  'Hello world!'
+require 'faker'
+get '/' do #cuando ingrese a la url hace lo siguiente
+  @name = Faker::Name.name #creamos una variable de instancia que podemos imprimir en los view
+  @libros = Faker::Book.title #estamos usando la gema faker para probar
+  @chewie = Faker::StarWars.wookiee_sentence # <3 mori de amor
+  @lol = Faker::LeagueOfLegends.quote
+  erb :index #estamos llamando a la vista index
 end
